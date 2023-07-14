16f072462523668ce219d93793d08a545e52548c
astrone1 <aztrone1@gmail.com>
2023-07-14 00:30:39 -0400

    mensaje

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as faceapi from 'face-api.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  
+  @ViewChild('videoContainer',{static:true}) videoContainer!:ElementRef;
+  
+  ngOnInit() {
+    this.startCamara();
+    this.playEvento();
+  }
+  
+  async startCamara(){
+
+    var video = await navigator.mediaDevices.getUserMedia({video:true});
+
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models');
+    await faceapi.nets.faceExpressionNet.loadFromUri('/assets/models');
+
+
+    let stream = this.videoContainer.nativeElement;
+    stream.srcObject = video;
+
+  }
+
+  playEvento(){
+   this.videoContainer.nativeElement.addEventListener('play', ()=>{
+
+       const canvas = faceapi.createCanvasFromMedia(this.videoContainer.nativeElement);
+
+       document.body.append(canvas)
+
+       const displaySize={width:this.videoContainer.nativeElement.width, height:this.videoContainer.nativeElement.height};
+       faceapi.matchDimensions(canvas,displaySize);
+
+       setInterval(async()=>{
+      
+      const detections = await faceapi.detectAllFaces(this.videoContainer.nativeElement, new faceapi.TinyFaceDetectorOptions())
+                                      .withFaceLandmarks()
+                                      .withFaceExpressions();
+
+
+      const resizeDetections=faceapi.resizeResults(detections,displaySize);
+      canvas.getContext('2d')?.clearRect(0,0,canvas.width,canvas.height);
+      faceapi.draw.drawDetections(canvas,resizeDetections);
+      faceapi.draw.drawFaceLandmarks(canvas,resizeDetections);
+      faceapi.draw.drawFaceExpressions(canvas,resizeDetections);
+
+
+       },500)
+
+   });
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ComponentNameComponent } from './component-name/component-name.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentNameComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
